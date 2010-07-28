484bb5fcdba847c7cebd4f55ac9746102b5c3039
shearn89 <alexshearn@thor.windows>
2010-07-28 15:03:28 +0100

    site up, need to add text to menu

--- scripts/homepage.js
+++ scripts/homepage.js
@@ -2,32 +2,48 @@
 $(document).ready(function(){
 	var cascaderGui = {
 		divid: '#cascader-gui',
+		textid: '#textcascader-gui',
 		state: 0,
 	};
 	var inthelabs = {
 		divid: '#inthelabs',
+		textid: '#textinthelabs',
 		state: 0,
 	};
 	var massprint = {
 		divid: '#massprint',
+		textid: '#textmassprint',
 		state: 0,
 	};
 	var mailer = {
 		divid: '#mailer',
+		textid: '#textmailer',
 		state: 0,
 	};
 	var ubuntuBg = {
 		divid: '#ubuntu-bg',
+		textid: '#textubuntu-bg',
 		state: 0,
 	};
 	var lolsoft = {
 		divid: '#lolsoft',
+		textid: '#textlolsoft',
 		state: 0,
 	};
 	
 	var divs = [];
 	divs.push(cascaderGui, inthelabs, massprint, mailer, ubuntuBg, lolsoft);
-	
+
+	function showNav(divTar)
+	{
+//		$(divTar).animate({visibility: 'visible', zIndex: '100'},{queue:false,duration:300});
+		$(divTar).fadeIn('300');
+	}
+	function hideNav(divTar)
+	{
+		$(divTar).fadeOut('300');
+	}
+
 	function bump(divTar, state)
 	{
 		if (state != 1){
@@ -48,5 +64,30 @@ $(document).ready(function(){
 		}).mouseout(function() {
 			bumpBack(item.divid, Number(item.state));
 		});
+	});
+	
+	jQuery.each(divs, function(i){
+		var item = divs[i];
+		$(item.divid).mouseup(function(){
+			if (Number(item.state)==0){
+				showNav(item.textid);
+				for (var j=0; j<6; j++){
+					if (j != i){
+						hideNav(divs[j].textid);
+						bumpBack(divs[j].divid, 0);
+						divs[j].state = 0;
+					}
+				}
+				item.state = 1;
+			}
+			else {
+				jQuery.each(divs, function(i){
+					var item = divs[i];
+					hideNav(item.textid);
+					bumpBack(item.divid, 0);
+					item.state = 0;
+				});
+		}
+		})
 	});
 });
